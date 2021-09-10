6d1df64439e0d4e325c07a6394ceb21bd4ca033e
jennica-wang <jennica.wang@education.nsw.gov.au>
2021-09-10 10:00:49 +1000

    10:02 10/09/21

--- myScript.js
+++ myScript.js
@@ -1,3 +1,23 @@
+function hideBannerClick() {
+    document.getElementById("banner_img").style.opacity = "0%"
+}
+
 function openIndexClick() {
-    document.getElementById("myIndexID").style.opacity = "100%";
+    document.getElementById("myIndexID").style.display = "inline";
+    document.getElementById("closeIndex").style.display = "inline";
+    document.getElementById("openIndex").style.display = "none";
+}
+
+function closeIndexClick() {
+    document.getElementById("myIndexID").style.display = "none";
+    document.getElementById("closeIndex").style.display = "none";
+    document.getElementById("openIndex").style.display = "inline";
+}
+
+function openContentsClick() {
+    document.getElementById("myContentsID").style.opacity = "100%";
+}
+
+function closeContentsClick() {
+    document.getElementById("myContentsID").style.opacity = "10%";
 }
