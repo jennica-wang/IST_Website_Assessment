686b4b5b617d125fc4666baf0aa7d734eae816fc
jennica-wang <jennica.wang@education.nsw.gov.au>
2021-09-23 12:33:36 +1000

    Update 23/09/21

--- myScript.js
+++ myScript.js
@@ -1,9 +1,11 @@
+// When the open index button is clicked, the index will be shown, the close index button will be shown and the open index button will be hidden.
 function openIndexClick() {
     document.getElementById("myIndexID").style.display = "inline";
     document.getElementById("closeIndex").style.display = "inline";
     document.getElementById("openIndex").style.display = "none";
 }
 
+// When the close index button is clicked, the index will be hidden, the close index button will be hidden and the open index button will be shown.
 function closeIndexClick() {
     document.getElementById("myIndexID").style.display = "none";
     document.getElementById("closeIndex").style.display = "none";
